fabe9892357be17be5bda963a3581aa4e9d58e08
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-06 22:28:10 -0600

    added books in table plus, have multiple librarian users

---
+++ libraryapp/urls.py
@@ -0,0 +1,12 @@
+# This is a lot like the ApplicationViews we used in React.
+# We are defining what view the user will be served up based on the url
+
+from django.urls import path
+from .views import *
+
+app_name = "libraryapp"
+
+urlpatterns = [
+    path('', book_list, name='home'),
+    path('books/', book_list, name='books'),
+]
