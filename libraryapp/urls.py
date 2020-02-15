10d8eb583a87b1d6dc2d42da6b0c2dea889d437f
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-15 14:51:38 -0600

    books now have details views a user can visit

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('books/form', book_form, name='book_form'),
     path('libraries/form', library_form, name='library_form'),
+    path('books/<int:book_id>/', book_details, name='book')
 ]
