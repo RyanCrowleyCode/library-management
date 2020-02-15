85aa054ec333805b7c57685279beabf3a322d803
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-15 15:00:57 -0600

    libraries now have detail views

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('books/form', book_form, name='book_form'),
     path('libraries/form', library_form, name='library_form'),
-    path('books/<int:book_id>/', book_details, name='book')
+    path('books/<int:book_id>/', book_details, name='book'),
+    path('libraries/<int:library_id>/', library_details, name='library')
 ]
